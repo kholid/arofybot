e3a0615eee611b91ddc94a4d8610a7032dff2dec
kholid <arrofi.kholid@gmail.com>
2018-02-22 19:39:56 +0700

    update db

--- arofybot.py
+++ arofybot.py
@@ -2,44 +2,76 @@ import os
 import telegram
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import replykeyboardmarkup
+from dbhelper import DBHelper
 
 
+db = DBHelper()
 TOKEN = os.environ['TELEGRAM_TOKEN']
 
 
 def start(bot, update):
-
     text = "Hi! I'm your personal assistant. What can I do for you?"
     update.message.reply_text(text)
 
 
 def help(bot, update):
-
-    text = "Some commands are still in progress, please wait"
+    text = "ArofyBot may help you with these commands:"
+    text += "\n\n/start - standard bot first command"
+    text += "\n/help - asking bot what he can do"
+    text += "\n/outcome <number> - list your outcome"
     update.message.reply_text(text)
 
 
 def echo(bot, update):
-
     chat_id = update.message.chat_id
     text = update.message.text
     bot.send_message(chat_id, text)
 
 
 def unknown(bot, update):
-
     chat_id = update.message.chat_id
-    text = "Bot can't understand your command, maybe Bot need to study more :D"
+    text = "can't understand your command, maybe Bot need to study more :D"
     bot.send_message(chat_id, text)
 
 
+def outcome(bot, update, args):
+    chat_id = update.message.chat_id
+    outlist = db.get_outcome(chat_id)
+    amount = args[0]
+    db.add_outcome(amount, chat_id)
+    text = "\n".join(outlist)
+    bot.send_message(text, chat_id)
+
+
+def totalOutcome(bot, update):
+    chat_id = update.message.chat_id
+    totalOut = db.get_total_outcome(chat_id)
+    bot.send_message(totalOut, chat_id)
+
+
+def askLocation(bot, update):
+    text = "Location is: " + update.message.latitude + " : " + update.message.longitude
+    #update.message.reply_location(text, reply_to_message_id=update.message.message_id)
+    bot.send_message(chat_id=update.message.chat_id, text, reply_markup=reply_markup)
+
+
 start_handler = CommandHandler('start', start)
-help_handler = CommandHandler('help', start)
+help_handler = CommandHandler('help', help)
 echo_handler = MessageHandler(Filters.text, echo)
 unknown_handler = MessageHandler(Filters.command, unknown)
+outcome_handler = CommandHandler('outcome', outcome, pass_args=True)
+total_handler = CommandHandler('totalOutcome', totalOutcome)
+location_handler = CommandHandler('askLocation', askLocation)
 
 
 def main():
+    location_keyboard = telegram.KeyboardButton(text="Send Location", request_location=True)
+    custom_keyboard = [["location keyboard, keyboard"]]
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+
+    db.setup()
+
     updater = Updater(TOKEN)
 
     dispatcher = updater.dispatcher
@@ -48,6 +80,9 @@ def main():
     dispatcher.add_handler(help_handler)
     dispatcher.add_handler(echo_handler)
     dispatcher.add_handler(unknown_handler)
+    dispatcher.add_handler(outcome_handler)
+    dispatcher.add_handler(total_handler)
+    dispatcher.add_handler(location_handler)
 
     updater.start_polling()
 
--- dbhelper.py
+++ dbhelper.py
@@ -3,32 +3,37 @@ import sqlite3
 
 class DBHelper:
 
-    def __init__(self, dbname="todo.sqlite"):
+    def __init__(self, dbname="outc.sqlite"):
         self.dbname = dbname
         self.conn = sqlite3.connect(dbname)
 
     def setup(self):
-        tblstmt = "CREATE TABLE IF NOT EXISTS items (description text, owner text)"
-        itemidx = "CREATE INDEX IF NOT EXISTS itemIndex ON items (description ASC)" 
-        ownidx = "CREATE INDEX IF NOT EXISTS ownIndex ON items (owner ASC)"
+        tblstmt = "CREATE TABLE IF NOT EXISTS outcome (amount number, owner text)"
+        outidx = "CREATE TABLE IF NOT EXISTS outcomeIndex ON outcome (amount ASC)"
+        ownidx = "CREATE TABLE IF NOT EXISTS ownIndex ON outcome (owner ASC)"
         self.conn.execute(tblstmt)
-        self.conn.execute(itemidx)
+        self.conn.execute(outidx)
         self.conn.execute(ownidx)
         self.conn.commit()
 
-    def add_item(self, item_text, owner):
-        stmt = "INSERT INTO items (description, owner) VALUES (?, ?)"
-        args = (item_text, owner)
+    def add_outcome(self, out_amount, owner):
+        stmt = "INSERT INTO outcome (amount, owner) VALUES (?, ?)"
+        args = (out_amount, owner)
         self.conn.execute(stmt, args)
         self.conn.commit()
 
-    def delete_item(self, item_text, owner):
-        stmt = "DELETE FROM items WHERE description = (?) AND owner = (?)"
-        args = (item_text, owner)
+    def delete_outcome(self, out_amount, owner):
+        stmt = "DELETE FROM outcome WHERE amount = (?) AND owner = (?)"
+        args = (out_amount, owner)
         self.conn.execute(stmt, args)
         self.conn.commit()
 
-    def get_items(self, owner):
-        stmt = "SELECT description FROM items WHERE owner = (?)"
+    def get_outcome(self, owner):
+        stmt = "SELECT amount FROM outcome WHERE owner = (?)"
+        args = (owner, )
+        return [x[0] for x in self.conn.execute(stmt, args)]
+
+    def get_total_outcome(self, owner):
+        stmt = "SELECT SUM(amount) FROM outcome WHERE owner = (?)"
         args = (owner, )
         return [x[0] for x in self.conn.execute(stmt, args)]
