735d2d722329a57bb083bd46f819736ce0d0925e
kholid <arrofi.kholid@gmail.com>
2018-02-22 09:53:54 +0700

    update using python telegram bot API

--- arofybot.py
+++ arofybot.py
@@ -1,101 +1,56 @@
-import json
-import requests
-import time
-import urllib
 import os
 
-from dbhelper import DBHelper
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-db = DBHelper()
 
 TOKEN = os.environ['TELEGRAM_TOKEN']
-URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
 
-def get_url(url):
-    response = requests.get(url)
-    content = response.content.decode("utf8")
-    return content
+def start(bot, update):
+
+    text = "Hi! I'm your personal assistant. What can I do for you?"
+    update.message.reply_text(text)
 
 
-def get_json_from_url(url):
-    content = get_url(url)
-    js = json.loads(content)
-    return js
+def help(bot, update):
+
+    text = "Some commands are still in progress, please wait"
+    update.message.reply_text(text)
 
 
-def get_updates(offset=None):
-    url = URL + "getUpdates"
-    if offset:
-        url += "?offset={}".format(offset)
-    js = get_json_from_url(url)
-    return js
+def echo(bot, update):
+
+    chat_id = update.message.chat_id
+    text = update.message.text
+    bot.send_message(chat_id, text)
 
 
-def get_last_update_id(updates):
-    update_ids = []
-    for update in updates["result"]:
-        update_ids.append(int(update["update_id"]))
-    return max(update_ids)
+def unknown(bot, update):
+
+    chat_id = update.message.chat_id
+    text = "Bot can't understand your command, maybe Bot need to study more :D"
+    bot.send_message(chat_id, text)
 
 
-def handle_updates(updates):
-    for update in updates["result"]:
-        text = update["message"]["text"]
-        chat = update["message"]["chat"]["id"]
-        items = db.get_items(chat)
-        if text == "/done":
-            keyboard = build_keyboard(items)
-            send_message("Select an item to delete", chat, keyboard)
-        elif text == "/start":
-            send_message("Welcome to your personal Assistant. Send any text to me and I'll store it as an item. Send /done to remove items", chat)
-        elif text == "/about":
-            send_message("Arofy's personal assistant", chat)
-        elif text.startswith("/"):
-            continue
-        elif text in items:
-            db.delete_item(text, chat)
-            items = db.get_items(chat)
-            keyboard = build_keyboard(items)
-            send_message("Select an item to delete", chat, keyboard)
-        else:
-            db.add_item(text, chat)
-            items = db.get_items(chat)
-            message = "\n".join(items)
-            send_message(message, chat)
-
-
-def get_last_chat_id_and_text(updates):
-    num_updates = len(updates["result"])
-    last_update = num_updates - 1
-    text = updates["result"][last_update]["message"]["text"]
-    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
-    return (text, chat_id)
-
-
-def build_keyboard(items):
-    keyboard = [[item] for item in items]
-    reply_markup = {"keyboard": keyboard, "one_time_keyboard": True}
-    return json.dumps(reply_markup)
-
-
-def send_message(text, chat_id, reply_markup=None):
-    text = urllib.parse.quote_plus(text)
-    url = URL + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chat_id)
-    if reply_markup:
-        url += "&reply_markup={}".format(reply_markup)
-    get_url(url)
+start_handler = CommandHandler('start', start)
+help_handler = CommandHandler('help', start)
+echo_handler = MessageHandler(Filters.text, echo)
+unknown_handler = MessageHandler(Filters.command, unknown)
 
 
 def main():
-    db.setup()
-    last_update_id = None
-    while True:
-        updates = get_updates(last_update_id)
-        if len(updates["result"]) > 0:
-            last_update_id = get_last_update_id(updates) + 1
-            handle_updates(updates)
-        time.sleep(0.5)
+    updater = Updater(TOKEN)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(help_handler)
+    dispatcher.add_handler(echo_handler)
+    dispatcher.add_handler(unknown_handler)
+
+    updater.start_polling()
+
+    updater.idle()
 
 
 if __name__ == '__main__':
